41e6809155efa8c80f6e1369adc41492ba151b93
beimer16 <beimerar16@gmail.com>
2019-08-27 18:19:42 -0500

    Update BaseRepository.cs

--- ApiWebZabud.Core/Repositories/BaseRepository/BaseRepository.cs
+++ ApiWebZabud.Core/Repositories/BaseRepository/BaseRepository.cs
@@ -32,58 +32,7 @@ namespace ApiWebZabud.Core.Repositories.BaseRepository
 
 
 
-        //#region IDisposable Support
-        //private bool disposedValue = false; // To detect redundant calls
 
-        //protected virtual void Dispose(bool disposing)
-        //{
-        //    if (!disposedValue)
-        //    {
-        //        if (disposing)
-        //        {
-        //            // TODO: dispose managed state (managed objects).
-        //        }
-
-        //        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-        //        // TODO: set large fields to null.
-
-        //        disposedValue = true;
-        //    }
-        //}
-
-        //// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        //// ~BaseRepository() {
-        ////   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        ////   Dispose(false);
-        //// }
-
-        //// This code added to correctly implement the disposable pattern.
-        //public void Dispose()
-        //{
-        //    // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //    Dispose(true);
-        //    // TODO: uncomment the following line if the finalizer is overridden above.
-        //    // GC.SuppressFinalize(this);
-        //}
-        //#endregion
-
-
-        //public sealed class Singleton
-        //{
-        //    private readonly static Singleton _instance = new Singleton();
-
-        //    private Singleton()
-        //    {
-        //    }
-
-        //    public static Singleton Instance
-        //    {
-        //        get
-        //        {
-        //            return _instance;
-        //        }
-        //    }
-        //}
 
     }
 }
